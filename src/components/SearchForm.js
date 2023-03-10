8cf03810e073151276ff3ce849be4726960efad6
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-03-10 20:11:32 +0530

    fetch drinks

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+  const {setSearchTerm} = useGlobalContext();
   return (
     <div>
       <h2>search form component</h2>
