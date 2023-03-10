9aa3c3f262ee20f8fd57a2b8855e4264fec0ba19
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-03-10 18:54:38 +0530

    about page

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam at scelerisque lorem. Donec nisl nibh, lobortis in ipsum egestas, rutrum porttitor lacus. Sed fermentum eget sem eu hendrerit. Phasellus luctus iaculis tortor. Etiam ut mattis sapien. Donec fringilla, eros eget ornare ornare, augue risus placerat dui, eget lacinia odio nisi ut urna. Sed ac fringilla purus, id mattis odio.
+
+Sed erat odio, congue et urna sit amet, dignissim ultricies mauris. Donec nec tincidunt lacus. Quisque non lacus in velit aliquet dapibus id cursus enim. Vestibulum condimentum erat eu purus rutrum, porttitor pellentesque neque vulputate. Aliquam erat volutpat. Donec faucibus posuere lorem, eget auctor libero tempus eu. Praesent pharetra massa nec est semper, sit amet sodales nibh feugiat. Phasellus ut sem aliquet, interdum leo vitae, tristique eros. Quisque molestie orci id arcu auctor tincidunt. Quisque vestibulum porta scelerisque. Nullam dictum tincidunt molestie. Aliquam risus orci, consectetur in volutpat dignissim, congue nec magna. Nunc eu justo tortor.
+
+Nullam bibendum congue ex non dictum. Suspendisse quis dapibus nulla. Aenean lobortis tempus auctor. Nulla bibendum sapien nulla, a pulvinar est laoreet vel. Nullam tincidunt nulla felis, ac dapibus nisi finibus non. Nullam efficitur velit nec augue gravida, et hendrerit dui lobortis. Cras pharetra nulla condimentum dui vehicula consectetur. Vivamus ac nisl tincidunt, ornare felis vel, consectetur orci.
+
+Donec magna felis, tincidunt a ultricies sit amet, lobortis cursus erat. Donec nisl eros, auctor eget dapibus interdum, malesuada quis felis. Ut nec dolor et nibh lacinia pellentesque nec sed tortor. Maecenas condimentum elit ut erat vehicula pellentesque. Etiam at cursus lacus, nec malesuada tortor. Etiam est leo, dapibus ut erat quis, egestas auctor felis. Integer eu est nec ligula elementum egestas sed ac est. Aliquam fringilla justo lacus, nec imperdiet ante luctus nec. Donec at fringilla tellus.
+      </p>
+    </section>
+  )
+}
+
+export default About
